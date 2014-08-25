55d4a2b72881b50c40bdf9cebe3f0e072e9c9e4c
Sebastian Müller <sebastian.mueller@red6-es.de>
2014-08-25 17:17:38 +0200

    Fix karma setup for codeclimate

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -47,7 +47,8 @@ module.exports = function(config) {
 
     coverageReporter: {
       type: 'lcov',
-      dir: 'coverage/'
+      dir: 'coverage/',
+      subdir: '.'
     },
 
     // testing framework to use (jasmine/mocha/qunit/...)
